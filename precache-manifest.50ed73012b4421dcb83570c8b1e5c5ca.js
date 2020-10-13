6a3d504b86bd6fbce8f8ecf10f2f9aa38857a9a1
rakaadi <56627679+rakaadi@users.noreply.github.com>
2020-10-13 14:27:14 +0700

    Updates

---
+++ precache-manifest.50ed73012b4421dcb83570c8b1e5c5ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b726c2eacb4c489908f19821771b7b71",
+    "url": "/furnize-landing-page/index.html"
+  },
+  {
+    "revision": "4aec4a8ffb7ac5741b10",
+    "url": "/furnize-landing-page/static/css/main.cf0041b2.chunk.css"
+  },
+  {
+    "revision": "0b4a80f129e3a68fd10f",
+    "url": "/furnize-landing-page/static/js/2.7a67c7cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/furnize-landing-page/static/js/2.7a67c7cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aec4a8ffb7ac5741b10",
+    "url": "/furnize-landing-page/static/js/main.aba417b4.chunk.js"
+  },
+  {
+    "revision": "23671865436925fb48d5",
+    "url": "/furnize-landing-page/static/js/runtime-main.0fd3aa63.js"
+  },
+  {
+    "revision": "4827e1e5b8e8abb5428ff68ef4b61f0a",
+    "url": "/furnize-landing-page/static/media/furnize-logo.4827e1e5.svg"
+  },
+  {
+    "revision": "635a1f2943694221792cd3cee51a21c2",
+    "url": "/furnize-landing-page/static/media/jacalyn-beales-edit.635a1f29.jpg"
+  }
+]);
