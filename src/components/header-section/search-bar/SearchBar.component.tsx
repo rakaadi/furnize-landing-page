3c8bede6175fd74a35c15482a9b0c966f63401d0
rakaadi <56627679+rakaadi@users.noreply.github.com>
2020-11-03 12:55:47 +0700

    Delete unused components and fix syntax not using camel case in SearchBar component

--- src/components/header-section/search-bar/SearchBar.component.tsx
+++ src/components/header-section/search-bar/SearchBar.component.tsx
@@ -6,7 +6,7 @@ const SearchBar = () => {
             <input type="search" name="search" id="search" placeholder="Search Your Dream Furniture Here.."
                 className="text-sm lg:text-base appearance-none w-full outline-none focus:outline-none active:outline-none" />
             <button type="submit" className="ml-1 outline-none focus:outline-none active:outline-none">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                     viewBox="0 0 24 24" className="w-6 h-6">
                     <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
                 </svg>
